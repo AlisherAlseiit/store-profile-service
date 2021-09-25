debece27e961fc91f7e4f379a1b00a05cd3cd1bd
Alisher Alseiit <Your Name>
2021-09-25 19:25:59 +0600

    profile microservice

---
+++ src/main/java/com/example/storeprofileservice/StoreProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreProfileServiceApplication.class, args);
+    }
+
+}
